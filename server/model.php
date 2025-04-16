a474ecca42d39694145a614c91c563c89607846d
Ethan <ethan.lochis@etu.unilim.fr>
2025-04-16 09:24:00 +0200

    SAE fin ité 12

--- server/model.php
+++ server/model.php
@@ -1,19 +1,5 @@
 <?php
 
-/**
- * Ce fichier contient toutes les fonctions qui réalisent des opérations
- * sur la base de données, telles que les requêtes SQL pour insérer, 
- * mettre à jour, supprimer ou récupérer des données.
- */
-
-/**
- * Définition des constantes de connexion à la base de données.
- *
- * HOST : Nom d'hôte du serveur de base de données, ici "localhost".
- * DBNAME : Nom de la base de données
- * DBLOGIN : Nom d'utilisateur pour se connecter à la base de données.
- * DBPWD : Mot de passe pour se connecter à la base de données.
- */
 define("HOST", "localhost");
 define("DBNAME", "lochis1");
 define("DBLOGIN", "lochis1");
